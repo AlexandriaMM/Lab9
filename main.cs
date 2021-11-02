66e5d988ab1524caf1e66fa879525a5785c5287c
AlexandriaMM <89824931+AlexandriaMM@users.noreply.github.com>
2021-11-02 00:32:11 +0000

    got rid of Hello World in main.cs

--- main.cs
+++ main.cs
@@ -2,7 +2,6 @@ using System;
 
 class Program {
   public static void Main (string[] args) {
-    //Console.WriteLine ("Hello World");
     Employee employee1 = new Employee();
     employee1.Intro();
     Employee employee2 = new Employee("John", "Smith", "1234", 45);
