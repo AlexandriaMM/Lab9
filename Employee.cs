48a31529ffb17a37d22f0a649961d66b403e4546
AlexandriaMM <89824931+AlexandriaMM@users.noreply.github.com>
2021-11-08 07:03:16 +0000

    Lab 10 changes added

--- Employee.cs
+++ Employee.cs
@@ -1,5 +1,6 @@
 using System;
 class Employee {
+  //fields
   private string firstname;
   private string lastname;
   private string id;
@@ -10,31 +11,96 @@ class Employee {
   private double yearlysalary;
   private string employmentstatus;
 
+  //properties are where you add validation via getters and setters
+  //properties are public and names are uppercase
+  public string Firstname {get; set;}
+  public string Lastname {get; set;}
+  public int Age {
+    get{ return age; } 
+    set{
+        if(value > 18){
+          age = value; 
+        }
+        else {
+          age = 18;
+          //Console.WriteLine("Age is set to 18");
+        }
+      }
+    }
+  public double Yearlysalary { 
+      get{ return yearlysalary; }
+      set{
+        if(value >= 1000){
+          yearlysalary = value;
+        }
+        else {
+          yearlysalary = 1000;
+          //Console.WriteLine("Yearly salary is set to 1000"); 
+        }
+      }
+    }
+    public string Id {get; set;}
+    public string Employmentstatus {get; set;}
+
   //constructor with no parameters
   public Employee()
   {
-    firstname = "Unknown";
-    lastname = "Unknown";
-    id = "Unknown";
+    Firstname = "unknown";
+    Lastname = "unknown";
     age = 0;
-    employmentstatus = "active";
-    Console.WriteLine("A new employee object has been created");
+    Id = "unknown";
+    Employmentstatus = "active";
+    //Console.WriteLine("A new employee has been created");
   }
 
 //constructor with parameters
   public Employee(string first, string last, string empid, int empage)
   {
-    firstname = first;
-    lastname = last;
-    id = empid;
-    age = empage;
-    employmentstatus = "active";
-    Console.WriteLine("A new employee object has been created");
+    Firstname = first;
+    Lastname = last;
+    Id = empid;
+    Age = empage;
+    Yearlysalary = 1000;
+    Employmentstatus = "active";
+    //Console.WriteLine("A new employee has been created");
   }
 
+  
   public void Intro()
   {
-    Console.WriteLine("First name: " + firstname + "\nLast name: " + lastname + "\nEmployement status: " + employmentstatus);
+    if(Firstname == "unknown")
+    {
+      Console.WriteLine("First name: " + Firstname + "\nLast name: " + Lastname + "\nAge: " + Age + "\nId: " + Id);
+    }
+    else
+    {
+      Console.WriteLine("First name: " + Firstname + "\nLast name: " + Lastname + "\nAge: " + Age + "\nId: " + Id + "\nYearly Salary: " + Yearlysalary);
+    }
   }
 
+  public void IncreaseSalary(double percent)
+  {
+    if(percent > 0)
+    {
+      Yearlysalary = ((percent*.01)*Yearlysalary)+Yearlysalary;
+      Console.WriteLine("Yearly salary is now " + Yearlysalary);
+    }
+    else
+    {
+      Console.WriteLine("Invalid input. Yearly Salary not updated");
+    }
+  }
+
+  public void RemoveEmployee()
+  {
+    if(Employmentstatus == "active")
+    {
+      Employmentstatus = "inactive";
+      Console.WriteLine("Employee removed successfully!");
+    }
+    else
+    {
+      Console.WriteLine("Employee already inactive");
+    }
+  }
 }
--- main.cs
+++ main.cs
@@ -2,9 +2,28 @@ using System;
 
 class Program {
   public static void Main (string[] args) {
+    //employee using constructor with no parameters
     Employee employee1 = new Employee();
     employee1.Intro();
-    Employee employee2 = new Employee("John", "Smith", "1234", 45);
+    
+    //employee using constructer with parameters
+    //testing Increase Salary Method
+    Employee employee2 = new Employee("Robert", "Downey", "1", 18);
     employee2.Intro();
+
+    //employee using constructer with parameters
+    //testing RemoveEmployee method
+    Employee employee3 = new Employee("John", "Smith", "S0089", 38);
+    employee3.Yearlysalary = 50000;
+    employee3.Intro();
+    employee3.IncreaseSalary(-2);
+    employee3.RemoveEmployee();
+
+    //employee using constructor with parameters
+    //testing Increase Salary
+    Employee employee4 = new Employee ("Maria", "Lambert", "S0010", 26);
+    employee4.IncreaseSalary(7);
+    employee4.RemoveEmployee();
+    employee4.RemoveEmployee();
   }
 }
